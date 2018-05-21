eeca122b1fef24255f545bb1a2b286b196dae935
Unknown <fwlodarezack@sqli.com>
2018-05-21 22:08:28 +0200

    fix : implement proper Clock component

--- src/components/clock/clock.jsx
+++ src/components/clock/clock.jsx
@@ -1,22 +1,48 @@
-import React from 'react';
-import { observer } from 'mobx-react';
+import React, { Component } from 'react';
+import moment from 'moment';
+import { capitalize } from '../../utils/string.utils';
 
-const Clock = ({ store }) => (
-  <div id='date_time'>
-    <div id='date' className='semi-arc e4'>
-      <div className='semi-arc-2 e4-1'>
-        <div className='counterspin4'></div>
+class Clock extends Component {
+
+  constructor(props) {
+    super(props);
+    moment.locale('en');
+    this.state = this.formatDate(moment());
+  }
+
+  formatDate = (m) => ({
+    hours: m.format('HH'),
+    minutes: m.format('mm'),
+    seconds: m.format('ss'),
+    dayName: capitalize(m.format('dddd')),
+    dayOfMonth: m.format('Do'),
+    monthName: capitalize(m.format('MMMM'))
+  });
+
+  componentWillMount = () => this.intervalID = setInterval(() => {
+    this.setState(this.formatDate(moment()));
+  }, 1000);
+
+  componentWillUnmount = () => clearInterval(this.intervalID);
+
+  render = () => (
+    <div id='date_time'>
+      <div id='date' className='semi-arc e4'>
+        <div className='semi-arc-2 e4-1'>
+          <div className='counterspin4'></div>
+        </div>
+        <div className='day-number'>{this.state.dayOfMonth}</div>
+        <div className='month'>{this.state.monthName}</div>
       </div>
-      <div className='day-number'>{store.dayOfMonth}</div>
-      <div className='month'>{store.monthName}</div>
-    </div>
 
-    <div id='time' className='arc e1'>
-      <div className='hour-minutes'>{`${store.hours}:${store.minutes}`}</div>
-      <div className='seconds'>{store.seconds}</div>
-      <div className='day-name'>{store.dayName}</div>
+      <div id='time' className='arc e1'>
+        <div className='hour-minutes'>{`${this.state.hours}:${this.state.minutes}`}</div>
+        <div className='seconds'>{this.state.seconds}</div>
+        <div className='day-name'>{this.state.dayName}</div>
+      </div>
     </div>
-  </div>
-);
+  );
 
-export default observer(Clock);
+}
+
+export default Clock;
--- src/components/clock/clock.spec.js
+++ src/components/clock/clock.spec.js
@@ -16,26 +16,9 @@ afterEach(() => {
 
 describe('Clock', () => {
   test('should render', () => {
-    const store = {
-      dayOfMonth: '1st',
-      monthName: 'January',
-      hours: '00',
-      minutes: '00',
-      seconds: '00',
-      dayName: 'Monday'
-    };
 
     const component = renderer.create(
-      <Clock store={store} />
+      <Clock />
     ).toJSON();
-  })
-
-  test('should fail rendering', () => {
-    expect(() => {
-      const component = renderer.create(
-        <Clock />
-      ).toJSON();
-    }).toThrow(TypeError);
-    expect(spy).toHaveBeenCalledTimes(1);
   })
 });
