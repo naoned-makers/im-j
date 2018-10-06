5db249c5855f130d330a700762ecd5f2e3657497
rguillome <dev@guillaumerenaudin.com>
2018-10-06 12:06:04 +0200

    Guillaume fait du React pour afficher le visage choisit par IronMan

--- src/components/camera/camera.jsx
+++ src/components/camera/camera.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 const Camera = ({ store }) => (
-  <img src={"data:image/jpeg;charset=utf-8;base64,"+store.img} className="w3-left w3-circle w3-margin-right " width="500px" height="375px" />
+  <img src={"data:image/jpeg;base64,"+store.img} className="w3-left w3-circle w3-margin-right" id="camera_frame" />
 );
 
 export default observer(Camera);
