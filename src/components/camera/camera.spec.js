bdf59623bbf6aaceee0d1b4375ecfcbf6871f730
rguillome <dev@guillaumerenaudin.com>
2018-10-06 12:12:11 +0200

    oops i did it again, forgot to correct tests

--- src/components/camera/camera.spec.js
+++ src/components/camera/camera.spec.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Kinect } from './';
+import { Camera } from './';
 import renderer from 'react-test-renderer';
 import ReactShallowRenderer from 'react-test-renderer/shallow';
 
 describe('Camera frame', () => {
   test('should render', () => {
+    const store = {
+      img: ''
+    };
+
     const component = renderer.create(
-      <Camera />
+      <Camera store={store}/>
     ).toJSON();
 
     expect(component.type).toBe('img');
