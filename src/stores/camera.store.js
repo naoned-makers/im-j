e55a82f02612b87d2110be440fcbf22bf6b6668b
rguillome <dev@guillaumerenaudin.com>
2018-10-06 12:20:17 +0200

    ok I'ld play the rules francky

--- src/stores/camera.store.js
+++ src/stores/camera.store.js
@@ -1,19 +1,19 @@
-import { observable, action } from 'mobx';
+import { observable } from 'mobx';
 import { client } from '../services/camera.service';
 
+const cameraStore = observable({
+  img: null
+});
 
 client.on('message', (topic, payload) => {
   const data = JSON.parse(payload);
   switch (topic) {
     case 'im/command/head/facetrackstart':
       cameraStore.img = data.face;
+      break;
     default:
       break;
   }
-});
-
-const cameraStore = observable({
-  img: null
 });
 
 export default cameraStore;
