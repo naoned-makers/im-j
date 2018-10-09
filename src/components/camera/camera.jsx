18071bd8052035a1fe3b9f6d9122c91faf9122b1
rguillome <dev@guillaumerenaudin.com>
2018-10-09 17:11:02 +0200

    take francky's advices into account

--- src/components/camera/camera.jsx
+++ src/components/camera/camera.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { observer } from 'mobx-react';
 
 const Camera = ({ store }) => (
-  <img src={'data:image/jpeg;base64,'+store.img} className="w3-left w3-circle w3-margin-right" id="camera_frame" />
+  <img src={`data:image/jpeg;base64,${store.img}`} className="w3-left w3-circle w3-margin-right" id="camera_frame" />
 );
 
 export default observer(Camera);
